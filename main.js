6bbfaea14a3086886660cb840a98fb56a0312daa
Raj298 <71762923+Raj298@users.noreply.github.com>
2021-02-07 11:49:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,26 +3,35 @@ status="";
 object=[];
 function preload(){
 img=loadImage('dog_cat.jpg')
+
 }
 
 function setup(){
-canvas=createCanvas(640,420)
+canvas=createCanvas(380,380)
 canvas.center();
 objectDetector=ml5.objectDetector('cocossd',modelLoaded)
 document.getElementById("status").innerHTML="Status:Detecting Objects";
-
+video=createCapture(VIDEO)
+video.hide()
+video.size(380,380)
 }
 
 function draw(){
-image(img,0,0,640,420)
+image(video,0,0,380,380)
 if(status!=""){
+    r=random(255)
+    g=random(255)
+    b=random(255)
+    objectDetector.detect(video,gotResults)
     for(i=0;i<object.length;i++){
-        fill('#ff0000')
+        document.getElementById("status").innerHTML="Status: Object Detected"
+        document.getElementById("number_of_objects").innerHTML="Number of objects detected:" +object.length
+        fill(r,g,b)
         percentage=floor(object[i].confidence*100)
 text(object[i].label +" "+percentage+"%",object[i].x+20,object[i].y+20)
 
 noFill()
-stroke('#ff0000')
+stroke(r,g,b)
 rect(object[i].x,object[i].y,object[i].width,object[i].height)
     }
 
@@ -32,7 +41,7 @@ rect(object[i].x,object[i].y,object[i].width,object[i].height)
 function modelLoaded(){
     console.log("modelLoaded")
     status="true"
-    objectDetector.detect(img,gotResults)
+   
 }
 function gotResults(error,results){
     if(error){
